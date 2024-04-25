2bd2cb88f6ac407dd8e6cd6b797b26a355417d70
Jerome White <jerome@thewex.net>
2024-04-25 18:10:43 +0530

    Remove get school functionality
    The ground truth should be taken from the targets, not a sheet that is
    potentially a superset of the targets.

--- get-responses.py
+++ get-responses.py
@@ -1,10 +1,58 @@
 import os
 import sys
+import functools as ft
+from pathlib import Path
 from argparse import ArgumentParser
+from urllib.parse import ParseResult, urlunparse
 
 import pandas as pd
+from googleapiclient.discovery import build
 
-from myutils import sheets
+from myutils import Logger
+
+#
+#
+#
+@ft.cache
+def _url():
+    kwargs = { x: None for x in ParseResult._fields }
+    p_result = ParseResult(**kwargs)
+
+    return p_result._replace(
+        scheme='https',
+        netloc='docs.google.com',
+    )
+
+def url(sheet_id, doc_id):
+    path = Path(
+        '/',
+        'spreadsheets',
+        'd',
+        sheet_id,
+        'export',
+    )
+    q_parts = {
+        'format': 'csv',
+        'gid': doc_id,
+    }
+    query = '&'.join(map('='.join, q_parts.items()))
+
+    target = _url()
+    target = target._replace(path=str(path), query=query)
+
+    return urlunparse(target)
+
+def sheets(sheet_id, token):
+    service = build('sheets', 'v4', developerKey=token)
+    gsheet = (service
+              .spreadsheets()
+              .get(spreadsheetId=sheet_id)
+              .execute())
+    assert sheet_id == gsheet['spreadsheetId']
+
+    for s in gsheet.get('sheets'):
+        doc_id = str(s['properties']['sheetId'])
+        yield url(sheet_id, doc_id)
 
 def extract(urls):
     columns = {
@@ -18,6 +66,7 @@ def extract(urls):
               .read_csv(u)
               .filter(items=columns))
         if len(df.columns) == ncol:
+            Logger.info(u)
             yield df.rename(columns=columns)
 
 if __name__ == "__main__":
--- myutils.py
+++ myutils.py
@@ -1,46 +1,13 @@
-import functools as ft
-from pathlib import Path
-from urllib.parse import ParseResult, urlunparse
+import os
+import logging
 
-from googleapiclient.discovery import build
-
-@ft.cache
-def _url():
-    kwargs = { x: None for x in ParseResult._fields }
-    p_result = ParseResult(**kwargs)
-
-    return p_result._replace(
-        scheme='https',
-        netloc='docs.google.com',
-    )
-
-def url(sheet_id, doc_id):
-    path = Path(
-        '/',
-        'spreadsheets',
-        'd',
-        sheet_id,
-        'export',
-    )
-    q_parts = {
-        'format': 'csv',
-        'gid': doc_id,
-    }
-    query = '&'.join(map('='.join, q_parts.items()))
-
-    target = _url()
-    target = target._replace(path=str(path), query=query)
-
-    return urlunparse(target)
-
-def sheets(sheet_id, token):
-    service = build('sheets', 'v4', developerKey=token)
-    gsheet = (service
-              .spreadsheets()
-              .get(spreadsheetId=sheet_id)
-              .execute())
-    assert sheet_id == gsheet['spreadsheetId']
-
-    for s in gsheet.get('sheets'):
-        doc_id = str(s['properties']['sheetId'])
-        yield url(args.sheet_id, doc_id)
+#
+#
+#
+logging.basicConfig(
+    format='[ %(asctime)s %(levelname)s %(filename)s ] %(message)s',
+    datefmt='%H:%M:%S',
+    level=os.environ.get('PYTHONLOGLEVEL', 'WARNING').upper(),
+)
+logging.captureWarnings(True)
+Logger = logging.getLogger(__name__)
